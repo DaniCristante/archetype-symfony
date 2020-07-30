2a229013fd0809b6d37487749d8a279ce077bce2
Daniel <daniel@runroom.com>
2020-07-30 15:11:08 +0200

    paginator on controller

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,23 +5,33 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\PageRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PageController extends AbstractController
 {
     protected $pageRepository;
+    protected $paginator;
 
-    public function __construct(PageRepository $pageRepository)
-    {
+    public function __construct(
+        PageRepository $pageRepository,
+        PaginatorInterface $paginator
+    ){
+        //Usar PageService y RendererService
         $this->pageRepository = $pageRepository;
+        $this->paginator = $paginator;
     }
 
-    /**
-     * @Route("/page", name="page")
-     */
-    public function index()
+    public function index(Request $request)
     {
+        //mover llamada repository a PageService, retornar modelo.
+        $queryBuilder = $this->pageRepository->getPagesQueryBuilder();
+        
+        $pagination = $this->paginator->paginate($queryBuilder, $request->get('page'), 5);
+
         return $this->render('page/index.html.twig', [
             'controller_name' => 'PageController',
+            'pagination' => $pagination
         ]);
     }
 }
--- src/Repository/PageRepository.php
+++ src/Repository/PageRepository.php
@@ -3,57 +3,22 @@
 namespace App\Repository;
 
 use App\Entity\Page;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
 
-/**
- * @method Page|null find($id, $lockMode = null, $lockVersion = null)
- * @method Page|null findOneBy(array $criteria, array $orderBy = null)
- * @method Page[]    findAll()
- * @method Page[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
- */
-class PageRepository extends ServiceEntityRepository
+class PageRepository 
 {
     protected $entityManager;
 
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        parent::__construct($registry, Page::class);
+        $this->entityManager = $entityManager;
     }
 
     public function getPagesQueryBuilder()
     {
-        return $this->createQueryBuilder()
+        return $this->entityManager->createQueryBuilder()
             ->select('page')
-            ->from('PageRepository::class', 'page');
+            ->from('App\Entity\Page', 'page');
     }
-    
-    // /**
-    //  * @return Page[] Returns an array of Page objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Page
-    {
-        return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
